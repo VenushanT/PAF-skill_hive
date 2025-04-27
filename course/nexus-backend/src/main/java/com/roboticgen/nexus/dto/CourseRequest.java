c8f0c80a6df91ed22f91bf398dfd46fcb5f5fd3a
abis029 <abishnanj@gmail.com>
2025-04-27 13:34:51 +0530

    Final Update

--- course/nexus-backend/src/main/java/com/roboticgen/nexus/dto/CourseRequest.java
+++
@@ -1,9 +0,0 @@
-package com.roboticgen.nexus.dto;
-
-import lombok.Data;
-
-@Data
-public class CourseRequest {
-    private String title;
-    private String description;
-}
--- course/nexus-backend/src/main/java/com/roboticgen/nexus/exception/CourseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.roboticgen.nexus.exception;
-
-public class CourseNotFoundException extends RuntimeException {
-    public CourseNotFoundException(String message) {
-        super(message);
-    }
-}

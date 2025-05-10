f2488e3897fbd4b108d25a22d6779ca633188079
Raagul <IT22130020@my.sliit.lk>
2025-05-11 02:07:31 +0530

    Post Like Model created

---
+++ backend/src/main/java/com/example/demo/model/PostInteraction.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "post_interactions")
+public class PostInteraction {
+    @Id
+    private String id;
+    private String recipientId; // The recipient (post creator)
+    private String postId;
+    private String type; // e.g., "LIKE", "COMMENT"
+    private String message;
+    private String commentId; // Add commentId for COMMENT type interactions
+    private boolean read;
+    private LocalDateTime createdAt;
+}
